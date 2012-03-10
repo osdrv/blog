598ef7a5a0974a0c27f4df4a30a222d8033d2713
4pcbr <i4pcbr@gmail.com>
2012-03-10 16:13:16 +0400

    title page css fixes

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,7 +1,12 @@
 class TopicsController < InheritedController
+  
   protected
 
   def collection
-    @topics ||= super.active.from_blog( @current_blog )
+    @topics ||= super.active.from_blog( @current_blog ).desc( :created_at )
+  end
+
+  def resource
+    @topic ||= resource_class.active.from_blog( @current_blog ).slugged( params[ :slug ] )
   end
 end
