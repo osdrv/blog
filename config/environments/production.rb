a8c310fbb9b4fb5f94d49e803780280fc37a1458
4pcbr <i4pcbr@gmail.com>
2012-03-11 00:00:25 +0400

    css fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,8 +14,7 @@ Blog::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 worker_processes 2
 working_directory "/var/www/4pcbr.com/"
-listen '127.0.0.1:7070', :tcp_nopush => true
+listen '127.0.0.1:7171', :tcp_nopush => true
 timeout 60
 pid "/var/www/4pcbr.com/tmp/unicorn.pid"
