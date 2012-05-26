bbfc42277d40d6c959d35a5b865c6479ef1131d0
4pcbr <o.s.sidorov@gmail.com>
2012-05-26 04:17:28 +0400

    fixed routes content type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,20 +14,18 @@ module ApplicationHelper
   end
   
   def markdown( text )
-    #options = [, :filter_html, , :no_intraemphasis, :fenced_code, :gh_blockcode]
     markdown_renderer.render( text ).html_safe
-    # syntax_highlighter(Redcarpet.new(text, *options).to_html).html_safe
   end
 
   def markdown_renderer
     @markdown_renderer ||= Redcarpet::Markdown.new( HTMLwithPygments, :fenced_code_blocks => true, :hard_wrap => true, :autolink => true )
   end
 
-  # def syntax_highlighter(html)
-  #   doc = Nokogiri::HTML(html)
-  #   doc.search("//pre[@lang]").each do |pre|
-  #     pre.replace Albino.colorize(pre.text.rstrip, pre[:lang])
-  #   end
-  #   doc.to_s
-  # end
+  def with_request_format( content_type )
+    current_format = request.format
+    request.format = content_type.to_sym
+    yield if block_given?
+    request.format = current_format
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Blog::Application.routes.draw do
   
   devise_for :jedis
 
-  resources :topics
-  match "topics(/tag/:tag)" => "topics#index", :as => :topics
-  match "topic/:slug" => "topics#show", :as => :topic
+  resources :topics, :defaults => { :format => :html }
+  match "topics(/tag/:tag)" => "topics#index", :as => :topics, :defaults => { :format => :html }
+  match "topic/:slug" => "topics#show", :as => :topic, :defaults => { :format => :html }
 
-  match "s/:slug" => "statics#show", :as => :static
+  match "s/:slug" => "statics#show", :as => :static, :defaults => { :format => :html }
 
   namespace :editor do
     resources :topics
@@ -16,9 +16,9 @@ Blog::Application.routes.draw do
     root :to => "title#index"
   end
   
-  match "p/:page", :to => "topics#index"
+  match "p/:page", :to => "topics#index", :defaults => { :format => :html }
   
-  root :to => "topics#index", :page => 1
+  root :to => "topics#index", :page => 1, :defaults => { :format => :html }
 
   match "*a", :to => "inherited#response_404"
 end
